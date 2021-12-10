36de09b517dc62cafca87e044e1bd2cdb8fd3aab
prijaymaharjan <prijaymaharjan@gmail.com>
2021-12-10 12:28:33 +0545

    Add Profiles

--- src/Component/TutorProfileblock.js
+++ src/Component/TutorProfileblock.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import personone from "../Images/person.jpg";
-
+import { Link } from "react-router-dom";
 class TutorProfileblock extends Component{
 render(){
     return(<div className="profile mt-5">
@@ -8,14 +8,12 @@ render(){
     <div className="d-flex justify-content-center row">
         <div className="col-md-10">
             <div className="row p-2 bg-white border rounded">
-                <div className ="col-md-3 mt-1"><img className="img-fluid img-responsive rounded product-image" src="https://i.imgur.com/QpjAiHq.jpg"/></div>
+                <div className ="col-md-3 mt-1"><img className="img-fluid imag img-responsive rounded product-image" src={personone}/></div>
                 <div className="col-md-6 mt-1">
                     <h5 className="name" id="name">Saroj Shrestha</h5>
                     <div className="d-flex flex-row">
                         <div className="ratings mr-2"><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i></div><span>310</span>
                     </div>
-                    {/* <div className="mt-1 mb-1 spec-1"><span>100% cotton</span><span className="dot"></span><span>Light weight</span><span className="dot"></span><span>Best finish<br/></span></div>
-                    <div className="mt-1 mb-1 spec-1"><span>Unique design</span><span className="dot"></span><span>For men</span><span className="dot"></span><span>Casual<br/></span></div> */}
                     <h6 className="mt-1 mb-1 spec-1 Qualification" id="Qualification">TU - BBS (Bachelors)</h6>
                     <p className="text-justify text-truncate para mb-0">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.<br/><br/></p>
                     {/* subject and Grade */}
@@ -40,9 +38,9 @@ render(){
                     </div>
                     {/* <h6 className="text-success">Free shipping</h6> */}
                     <div className="d-flex flex-column mt-4">
-                    
-                    <button className="btn btn-primary btn-sm viewProfile" id="viewProfile" type="button">Profile</button>
-                    <button className="btn btn-outline-primary btn-sm mt-2 orderTutor" id="orderTutor" type="button">Request Tutor</button></div>
+                    <Link to="#" className="a btn6 text-center viewProfile mt-2" id="viewProfile">Profile</Link>
+                    <Link to="#" className="a btn6 text-center  orderTutor mt-2" id="orderTutor">Request Tutor</Link>
+                   </div>
                 </div>
             </div>
             </div>
